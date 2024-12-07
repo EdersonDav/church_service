bd874443f50772cac466bc78b3f3c3f7f14735b7
edersondav <silva.edersonqueiroz@gmail.com>
2024-12-07 21:38:55 +0000

    rota hello world

--- src/server/http/controllers/controller.module.ts
+++ src/server/http/controllers/controller.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from '../../../core/core.module';
+import { HelloController } from './hello';
 
 @Module({
   imports: [CoreModule],
   controllers: [
+    HelloController
   ],
 })
 export class ControllerModule { }
